9a0acc3dab6868a1b81e5a938bc4cd44c2436d95
erinpwoo <erinpwoo@yahoo.com>
2017-06-11 05:39:35 -0700

    Full commit

--- bot.py
+++ bot.py
@@ -1,28 +1,13 @@
-# Copyright (c) 2015–2016 Molly White
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+
 
 import os
 import tweepy
 from secrets import *
 from time import gmtime, strftime
-
+import time
+import httplib
+import json
+from pprint import pprint
 
 # ====== Individual bot configuration ==========================
 bot_username = ''
@@ -31,10 +16,36 @@ logfile_name = bot_username + ".log"
 # ==============================================================
 
 
+
+
+conn = httplib.HTTPSConnection("api.themoviedb.org")
+
+payload = "{}"
+
+conn.request("GET", "/3/movie/latest?api_key=72a9e13ffcf773095ef24daba3324a34", payload)
+
+res = conn.getresponse()
+#data = res.read()
+info = json.load(res)
+moviedate = info["release_date"]
+moviename = info["title"]
+print moviename
+
+
+
+
+
+date = time.strftime("%x")
+
+systemdate = "20"+date[6:]+"-"+date[:2]+"-"+date[3:-3]
+print systemdate
+print moviedate
+
+
 def create_tweet():
     """Create the text of the tweet you want to send."""
     # Replace this with your code!
-    text = ""
+    text = "The movie '"+moviename+"' releases today ("+moviedate+") in theatres! *this is a test bot*" # Customized based on movie date
     return text
 
 
@@ -60,8 +71,7 @@ def log(message):
     with open(os.path.join(path, logfile_name), 'a+') as f:
         t = strftime("%d %b %Y %H:%M:%S", gmtime())
         f.write("\n" + t + " " + message)
+ #if moviedate == systemdate:
+tweet_text = create_tweet()
+tweet(tweet_text)
 
-
-if __name__ == "__main__":
-    tweet_text = create_tweet()
-    tweet(tweet_text)
